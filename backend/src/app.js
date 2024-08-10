69c9de06fbce5c48f8817f00053d09b2692fc21d
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-08-10 19:34:14 +0530

    created userRouter and ApiResponse class

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import userRouter from "./router/user.router.js";
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
+
+app.use("/api/v1/user",userRouter);
 
 
 
